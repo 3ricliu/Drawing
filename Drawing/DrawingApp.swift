9c844bbb5d74b4c3818b2324c6160a903955ff5a
Eric Liu <ericliu4943@gmail.com>
2020-10-25 17:59:21 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Eric Liu on 10/25/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
