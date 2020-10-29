a47c14827f795f3e89cc132afe294dfa98a7b3ce
Eric Liu <ericliu4943@gmail.com>
2020-10-28 22:43:20 -0700

    ColorCyclingRectangle and animated arrow bottom

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,52 +7,139 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
+//struct Trapezoid: Shape {
+//  var insetAmount: CGFloat
+//
+//  var animatableData: CGFloat {
+//    get { insetAmount }
+//    set { self.insetAmount = newValue }
+//  }
+//
+//  func path(in rect: CGRect) -> Path {
+//    var path = Path()
+//
+//    path.move(to: CGPoint(x: 0, y: rect.maxY))
+//    path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+//    path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+//    path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//    path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+//
+//    return path
+//  }
+//}
+
+struct ColorCyclingRectangle: View {
+  var amount = 0.0
+  var steps = 100
+  
+  var body: some View {
+    ZStack {
+      ForEach(0..<steps) { value in
+        Rectangle()
+          .inset(by: CGFloat(value))
+          .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+      }
+    }
+  }
+
+  func color(for value: Int, brightness: Double) -> Color {
+    var targetHue = Double(value) / Double(self.steps) + self.amount
     
-    path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-    path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-    path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-    path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+    if targetHue > 1 {
+      targetHue -= 1
+    }
     
-    return path
+    return Color(hue: targetHue, saturation: 1, brightness: brightness)
   }
 }
 
-struct Arc: InsettableShape {
-  var startAngle: Angle
-  var endAngle: Angle
-  var clockwise: Bool
+struct Rectangle: InsettableShape {
   var insetAmount: CGFloat = 0
   
   func path(in rect: CGRect) -> Path {
-    let rotationAdjustment = Angle.degrees(90)
-    let modifiedStart = startAngle - rotationAdjustment
-    let modifiedEnd = endAngle - rotationAdjustment
     var path = Path()
-    path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-    
+
+    path.move(to: CGPoint(x: 100, y: 300))
+    path.addLine(to: CGPoint(x: 100 + insetAmount, y: 300 + insetAmount))
+    path.addLine(to: CGPoint(x: 200 + insetAmount, y: 300  + insetAmount))
+    path.addLine(to: CGPoint(x: 200 + insetAmount, y: 0 + insetAmount))
+    path.addLine(to: CGPoint(x: 100 + insetAmount, y: 0 + insetAmount))
+    path.addLine(to: CGPoint(x: 100 + insetAmount, y: 300 + insetAmount))
+  
     return path
   }
   
   func inset(by amount: CGFloat) -> some InsettableShape {
-    var arc = self
-    arc.insetAmount += amount
-    return arc
+    var rect = self
+    rect.insetAmount += amount
+    return rect
+  }
+}
+
+struct Arrow: Shape {
+
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    
+    path.move(to: CGPoint(x: 0, y: rect.maxY))
+    path.addLine(to: CGPoint(x: 100, y: rect.minY))
+    path.addLine(to: CGPoint(x: 200, y: rect.maxY))
+
+    return path
+  }
+}
+
+struct AdjustableRectangle: Shape {
+  var insetAmount: CGFloat
+  
+  var animatableData: CGFloat {
+    get { insetAmount }
+    set { self.insetAmount = newValue }
+  }
+
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+
+    path.move(to: CGPoint(x: 100, y: 0))
+    path.addLine(to: CGPoint(x: 100, y: rect.maxY + insetAmount))
+  
+    return path
   }
 }
 
 struct ContentView: View {
-    var body: some View {
-      Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-        .strokeBorder(Color.blue, lineWidth: 40)
+  @State private var insetAmount: CGFloat = 0
+  @State private var colorCycle = 0.0
+
+  var body: some View {
+//    Trapezoid(insetAmount: insetAmount)
+//      .frame(width: 200, height: 100)
+//      .onTapGesture {
+//        withAnimation {
+//          self.insetAmount = CGFloat.random(in: 10...90)
+//        }
+//      }
+    VStack {
+      Arrow()
+        .frame(width: 200, height: 100)
+      AdjustableRectangle(insetAmount: insetAmount)
+        .stroke(Color.black, style: StrokeStyle(lineWidth: 50 + insetAmount))
+        .frame(width: 200, height: 100)
+        .onTapGesture {
+          withAnimation{
+            self.insetAmount += 10
+          }
+        }
+      ColorCyclingRectangle(amount: self.colorCycle)
+        .frame(width: 300, height: 300)
       
+      Slider(value: $colorCycle)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
